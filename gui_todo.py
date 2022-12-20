3a8deeb532250c6f0f21b98bd1a6d9c1519381ac
Nanda Kishore <bellamnanda1995@gmail.com>
2022-12-20 15:56:36 +0100

    Added complete and exit buttons

--- gui_todo.py
+++ gui_todo.py
@@ -2,24 +2,24 @@ import main_functions
 import PySimpleGUI as sg
 import main_functions
 
-label = sg.Text("Type in your todo")
+label = sg.Text("Type in your To-Do:", font="Helvetica 13 bold")
 input_box = sg.InputText(tooltip="Enter todo", key="To-Do")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=main_functions.get_todos(), key="todos", enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
-
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit", border_width=2, button_color="red", font="bold")
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box], [edit_button, complete_button, sg.Push(), exit_button]],
                    font=("Helvetica", 13))
 
 while True:
     event, values = window.read()
     # event is the label of the button pushed
     # values are the input given by the user during that event.
-    print(event)
-    print(values)
-
     match event:
         case "Add":
             todos = main_functions.get_todos()
@@ -30,7 +30,6 @@ while True:
             window["To-Do"].update(value=[])
 
         case "Edit":
-            print(values)
             edit_todo = values["todos"][0]
             todos = main_functions.get_todos()
             edit_idx = todos.index(edit_todo)
@@ -39,6 +38,18 @@ while True:
             main_functions.write_todo_file(todos)
             window["todos"].update(values=todos)
             window["To-Do"].update(value=[])
+
+        case "Complete":
+            todo_complete = values["todos"][0]
+            todos = main_functions.get_todos()
+            complete_idx = todos.index(todo_complete)
+            todos.pop(complete_idx)
+            main_functions.write_todo_file(todos)
+            window["todos"].update(values=todos)
+            window["To-Do"].update(value=[])
+
+        case "Exit":
+            break
 
         case "todos":
             window["To-Do"].update(value=values["todos"][0])
